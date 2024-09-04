f68728b2353e20a224b642e687d5beebfcb21b22
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:53:45 +0530

    Add files via upload

---
+++ multi_comp_operator.py
@@ -0,0 +1,7 @@
+age = 31
+name = "Gauri Hole"
+
+if age >= 18 and name=="Gauri_Hole":
+    print("\t \t Your Step towards the Success Is Starts !")
+else:
+    print("\t \t Your Age for Study Now !")
