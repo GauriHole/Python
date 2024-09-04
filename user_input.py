562a40862522a06412579800f3e4526723b34a2d
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:58:10 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("what is your name ? ")
+age = input("how old are you ?")
+dog_age = int(age) * 4
+print(f" \t\t My Self {name} ,\t I am {age} Years Old . My Dog Age is {dog_age} \n \t\t That's all , Thank You !")
