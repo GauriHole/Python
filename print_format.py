1a416ec9329ebea2ba183f1214358c8b827ef20d
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:54:33 +0530

    Add files via upload

---
+++ print_format.py
@@ -0,0 +1,9 @@
+name = "Gauri Hole"
+
+# Type - 01
+welcome_msg = "Hello {} welcome to Python 101".format(name)
+print(welcome_msg)
+
+# Type - 02
+welcome_msg2 = f"\t I am really serious about chocalates that's why i called {name} ,\n \t \t  Thank You !"
+print(welcome_msg2)
