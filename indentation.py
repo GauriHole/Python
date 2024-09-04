45aea3b6646117aa9d30646f81798838127559cb
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:51:52 +0530

    Add files via upload

---
+++ indentation.py
@@ -0,0 +1,8 @@
+course = "Python 101"
+twitter = "@GauriHole"
+
+if twitter == "@GauriHole":
+    name = "Gauri Hole"
+    instagram = "@hole_gauri_r"
+print(f"\n \n Hello {name} Your Instagram Account is Changed from {twitter} ... \n ")
+print(f"          * * * *    Login to Your Account As {instagram}    * * * *    ")
