c8ddd3965faf4259c7aa5c56b424ca89b12b0d9b
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:57:28 +0530

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,8 @@
+grocery_list = ['Apples','Orange','Banana','Pineapple']
+grocery_list =set(grocery_list)
+print(grocery_list)
+print(type(grocery_list))
+#Assignment
+grocery_list =tuple(grocery_list)
+print(grocery_list)
+print(type(grocery_list))
