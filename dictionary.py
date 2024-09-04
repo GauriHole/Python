2e41f9e85c19adab0fd9801061a24144efca2332
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:49:56 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+Person = {
+    "Id" : "G-12345",
+    "Name" : "Gauri Hole",
+    "Instagram" : "@hole_gauri_r",
+    "Email" : "gaurihole@gmail.com",
+    "Fav Hero" : "Ajinkya Raut"
+}
+print(Person)
+Person['Instagram']="@thoughts_of_gauri"
+print(Person)
